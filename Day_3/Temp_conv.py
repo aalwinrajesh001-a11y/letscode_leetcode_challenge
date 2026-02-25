a7d83e50f2f2057d48cff9c861ced7fc8e3bc97f
aalwinrajesh001-a11y <aalwinrajesh001@gmail.com>
2026-02-25 19:21:31 +0530

    Add temperature conversion class and method

--- Day_3/Temp_conv.py
+++ Day_3/Temp_conv.py
@@ -1,1 +1,7 @@
-
+class Solution:
+    def convertTemperature(self, celsius: float) -> List[float]:
+        k=float(f'{(celsius+273.15):.5f}')
+        f=float(f'{(celsius*1.80+32.00):.5f}')
+        t=[k,f]
+        return t
+      
