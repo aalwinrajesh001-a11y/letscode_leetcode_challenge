8175b538fb8a6e403e94a6c8d9a345aa40782236
aalwinrajesh001-a11y <aalwinrajesh001@gmail.com>
2026-02-26 18:57:43 +0530

    Implement average calculation for specific even numbers
    Add a class Solution with method to calculate average of even numbers divisible by 6.

--- Day_4/Average of even nors in list/Code.py
+++ Day_4/Average of even nors in list/Code.py
@@ -1,1 +1,13 @@
-
+class Solution:
+    def averageValue(self, nums: List[int]) -> int:
+        cnt,sm=0,0
+        n=len(nums)
+        for i in range (n):
+            if nums[i]%6==0:
+                sm+=nums[i]
+                cnt+=1
+        if cnt!=0:
+            return int(sm/cnt)
+        else:
+            return 0
+          
