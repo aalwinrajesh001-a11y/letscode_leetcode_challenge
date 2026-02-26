88ca46fe8f0ca6ab94a88980f8891b76e5023758
aalwinrajesh001-a11y <aalwinrajesh001@gmail.com>
2026-02-26 18:57:23 +0530

    Implement returnToBoundaryCount method in Solution class
    Added a class Solution with a method to count returns to boundary.

--- Day_4/Ant_In_Boundary/Bound_count.py
+++ Day_4/Ant_In_Boundary/Bound_count.py
@@ -1,1 +1,10 @@
-
+class Solution:
+    def returnToBoundaryCount(self, nums: List[int]) -> int:
+        pos=0
+        count=0
+        for i in nums:
+            pos+=i
+            if pos==0:
+                count+=1
+        return count
+      
