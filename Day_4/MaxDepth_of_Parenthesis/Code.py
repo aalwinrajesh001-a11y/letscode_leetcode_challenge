ef29f906bb1f940587635f885de0171375c8bc80
aalwinrajesh001-a11y <aalwinrajesh001@gmail.com>
2026-02-26 18:58:57 +0530

    Implement maxDepth function to calculate depth of parentheses

--- Day_4/MaxDepth_of_Parenthesis/Code.py
+++ Day_4/MaxDepth_of_Parenthesis/Code.py
@@ -1,1 +1,14 @@
-
+class Solution:
+    def maxDepth(self, s: str) -> int:
+        count=0
+        max=0
+        n=len(s)
+        for i in range (n):
+            if s[i]=='(':
+                count+=1
+            elif s[i]==')':
+                count-=1
+            if count>max:
+                max=count
+        return max
+      
