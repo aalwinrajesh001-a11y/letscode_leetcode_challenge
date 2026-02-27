2661819d854927909d3c1b92ecabcc19d07736eb
aalwinrajesh001-a11y <aalwinrajesh001@gmail.com>
2026-02-27 20:15:10 +0530

    Update 3DivsorInt.py

--- Day_5/3DivsorInt.py
+++ Day_5/3DivsorInt.py
@@ -1,1 +1,12 @@
-
+class Solution:
+    def isThree(self, n: int) -> bool:
+        num=(sqrt(n))
+        if int(num)**2==n and n>2:
+            for i in range (2,int(num)):
+                if int(num)%i==0:
+                    return False
+            else:
+                return True
+        else:
+            return False
+          
